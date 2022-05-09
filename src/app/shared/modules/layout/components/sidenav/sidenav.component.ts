0c92d749483c812b59a46b04d9ca4fd314e87079
AdongoJr2 <dev.mosesadongo@gmail.com>
2022-05-09 14:01:45 +0300

    apply additional styling to sidenav

--- src/app/shared/modules/layout/components/sidenav/sidenav.component.ts
+++ src/app/shared/modules/layout/components/sidenav/sidenav.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import {
+  faHome,
+  faPeopleGroup,
+  faHandHoldingDollar,
+  faBlog,
+  faBookOpenReader,
+  faAddressBook
+} from '@fortawesome/free-solid-svg-icons';
+// import {
+//   faAddressBook
+// } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,6 +18,12 @@ import { NgbActiveOffcanvas } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  readonly faHome = faHome;
+  readonly faPeopleGroup = faPeopleGroup;
+  readonly faHandHoldingDollar = faHandHoldingDollar;
+  readonly faBlog = faBlog;
+  readonly faBookOpenReader = faBookOpenReader;
+  readonly faAddressBook = faAddressBook;
 
   constructor(
     private activeOffcanvas: NgbActiveOffcanvas
