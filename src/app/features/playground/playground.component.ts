68822a0b458461026d8aeca8d7df3344ee56186d
AdongoJr2 <dev.mosesadongo@gmail.com>
2022-05-09 10:06:52 +0300

    replace the single FormControl with a Form Group

--- src/app/features/playground/playground.component.ts
+++ src/app/features/playground/playground.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-playground',
@@ -8,19 +8,51 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class PlaygroundComponent implements OnInit {
 
-  constructor() { }
+  issueCloseForm = this.fb.group({
+    issueCloseComment: [null],
+    issueCloseRating: [null],
+    issueCloseRatingComment: [null],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
   }
 
-  ctrl = new FormControl(null, Validators.required);
+  onIssueCloseSubmit(): void {
+    const form = this.issueCloseForm;
+    form.markAllAsTouched();
+    if (form.invalid) return;
 
-  toggle() {
-    if (this.ctrl.disabled) {
-      this.ctrl.enable();
-    } else {
-      this.ctrl.disable();
+    let payload: any = {
+      // comment: form.value.issueCloseComment,
+      starRating: form.value.issueCloseRating,
+      comment: form.value.issueCloseRatingComment,
     }
+
+    console.log('Submit form with data: ', payload);
+
+  }
+
+  resetIssueCloseForm(): void {
+    this.issueCloseForm.reset();
+  }
+
+  get issueCloseComment(): AbstractControl | null {
+    return this.issueCloseForm.get('issueCloseComment')
+  }
+  get issueCloseRating(): AbstractControl | null {
+    return this.issueCloseForm.get('issueCloseRating')
+  }
+  get issueCloseRatingComment(): AbstractControl | null {
+    return this.issueCloseForm.get('issueCloseRatingComment')
+  }
+
+  // TODO: define once and reuse!
+  fieldHasErrors(field: AbstractControl): boolean {
+    return field?.invalid && (field?.dirty || field?.touched)
   }
 
 }
